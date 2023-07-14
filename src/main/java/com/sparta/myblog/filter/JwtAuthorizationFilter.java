e28e65bc91ad166022a10d70a741d834c73fb80a
chae0 <limchae098@gmail.com>
2023-07-15 00:29:20 +0900

    FIX : 필터 단에서 인증 후에 권한을 부여하는 과정을 try catch문으로 처리
    desc ; responseResult() 메서드 코드 리팩토링

--- src/main/java/com/sparta/myblog/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/myblog/filter/JwtAuthorizationFilter.java
@@ -1,12 +1,16 @@
-package com.sparta.myblog.security;
+package com.sparta.myblog.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.myblog.dto.RestApiResponseDto;
 import com.sparta.myblog.jwt.JwtUtil;
+import com.sparta.myblog.security.UserDetailsServiceImpl;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -16,9 +20,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@Slf4j(topic = "JWT 검증 및 인가")
+@Slf4j(topic = "JwtAuthorizationFilter : JWT 검증 및 인가")
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
@@ -31,26 +34,19 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
-
+        log.info("doFilterInternal() : ");
+        // 헤더에서 substring 해서 토큰 값 가져오기
         String tokenValue = jwtUtil.getJwtFromHeader(req);
 
-/*        if(tokenValue==null){
-            responseResult(res,400,"토큰이 없습니다.");
-            return;
-        }*/
-
-        if (StringUtils.hasText(tokenValue) ) { // 토큰이 있을 경우
-
+        if (StringUtils.hasText(tokenValue)) { // 토큰이 있을 경우
             if (!jwtUtil.validateToken(tokenValue)) { // 토큰 검증 실패 시
-                responseResult(res,400,"토큰이 유효하지 않습니다.");
+                responseResult(res,HttpStatus.BAD_REQUEST,"토큰이 유효하지 않습니다.");
                 return;
             }
-
-            Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
-
             try {
+                Claims info = jwtUtil.getUserInfoFromToken(tokenValue,res); // 토큰으로 사용자 정보 가져오기
                 setAuthentication(info.getSubject());
-            } catch (Exception e) {
+            } catch (Exception e) { // 만료되지 않은 토큰에 대한 유저 정보가 DB에 없을 경우 인가?
                 log.error(e.getMessage());
                 return;
             }
@@ -61,6 +57,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     // 인증 처리
     public void setAuthentication(String username) {
+        log.info("setAuthentication");
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         Authentication authentication = createAuthentication(username);
         context.setAuthentication(authentication);
@@ -70,23 +67,19 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     // 인증 객체 생성
     private Authentication createAuthentication(String username) {
+        log.info("createAuthentication");
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
 
     // Client 에 반환할 msg, status 세팅 메서드
-    private void responseResult(HttpServletResponse response, int statusCode, String message) throws IOException {
-        String jsonResponse = "{\"status\": " + statusCode + ", \"message\": \"" + message + "\"}";
-
-        // Content-Type 및 문자 인코딩 설정
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        // PrintWriter 를 사용하여 응답 데이터 전송
-        PrintWriter writer = response.getWriter();
-        writer.write(jsonResponse);
-        writer.flush();
+    private void responseResult(HttpServletResponse res, HttpStatus status, String message) throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        RestApiResponseDto dto = new RestApiResponseDto(status.value(), message);
+        ObjectMapper objectMapper = new ObjectMapper();
+        res.getWriter().write(objectMapper.writeValueAsString(dto));
     }
 
 
