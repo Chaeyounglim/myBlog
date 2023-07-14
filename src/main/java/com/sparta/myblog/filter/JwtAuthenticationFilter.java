0510bbe452451355b616976b742b2ea71955e3ee
chae0 <limchae098@gmail.com>
2023-07-15 00:26:23 +0900

    FIX : 로그인을 Post가 아닌 다른 Method로 요청시 예외처리 및 로그인 id,password 키값 다를 경우 예외처리
    desc : id와 패스워드를 UserRequestDto 클래스 멤버변수에 매핑하는 과정에서 해당 json 데이터에서 키값이 다를 경우 예외처리를 하지 않고 return null;을 시킴. -> 이를 Controller 단에서 할 예정

--- src/main/java/com/sparta/myblog/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/myblog/filter/JwtAuthenticationFilter.java
@@ -1,23 +1,25 @@
-package com.sparta.myblog.security;
+package com.sparta.myblog.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.myblog.dto.RestApiResponseDto;
 import com.sparta.myblog.dto.UserRequestDto;
 import com.sparta.myblog.entity.UserRoleEnum;
 import com.sparta.myblog.jwt.JwtUtil;
+import com.sparta.myblog.security.UserDetailsImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@Slf4j(topic = "로그인 및 JWT 생성")
+@Slf4j(topic = "JwtAuthenticationFilter : 로그인 및 JWT 생성")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final JwtUtil jwtUtil;
 
@@ -31,7 +33,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         if (!request.getMethod().equals(HttpMethod.POST.name()) ) {
             // 해당 url 로 들어온 요청의 Method 가 POST 가 아니라면
             try {
-                responseResult(response,400,"HTTP Method Error");
+                responseResult(response,HttpStatus.BAD_REQUEST,"Not Supported HTTP Method");
                 return null;
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -50,12 +52,13 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             );
         } catch (IOException e) {
             log.error(e.getMessage());
-            throw new RuntimeException(e.getMessage());
+            return null;
         }
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        log.info("successfulAuthentication() : 로그인 성공 및 사용자 정보 가져오기");
         String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
         UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
 
@@ -63,27 +66,22 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
 
         // 응답 상태 코드와 메시지 설정
-        responseResult(response, 200, "로그인 성공");
+        responseResult(response, HttpStatus.OK, "로그인 성공");
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
-        responseResult(response, 400, "회원을 찾을 수 없습니다.");
+        log.error("로그인 실패");
+        responseResult(response, HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다.");
     }
 
-
     // Client 에 반환할 msg, status 세팅 메서드
-    private void responseResult(HttpServletResponse response, int statusCode, String message) throws IOException {
-        String jsonResponse = "{\"status\": " + statusCode + ", \"message\": \"" + message + "\"}";
-
-        // Content-Type 및 문자 인코딩 설정
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        // PrintWriter 를 사용하여 응답 데이터 전송
-        PrintWriter writer = response.getWriter();
-        writer.write(jsonResponse);
-        writer.flush();
+    private void responseResult(HttpServletResponse res, HttpStatus status, String message) throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        RestApiResponseDto dto = new RestApiResponseDto(status.value(), message);
+        ObjectMapper objectMapper = new ObjectMapper();
+        res.getWriter().write(objectMapper.writeValueAsString(dto));
     }
 
 }
