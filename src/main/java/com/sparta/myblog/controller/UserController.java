94f359e0ac9b61a15ff9bb7898e6cd0d8586487a
chae0 <limchae098@gmail.com>
2023-07-25 22:18:34 +0900

    FEATURE : When signup add AOP Exception #8 -1

--- src/main/java/com/sparta/myblog/controller/UserController.java
+++ src/main/java/com/sparta/myblog/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.sparta.myblog.controller;
 
 
+import com.sparta.myblog.dto.RestApiResponseDto;
 import com.sparta.myblog.dto.UserRequestDto;
 import com.sparta.myblog.service.UserServiceImpl;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,12 +9,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,26 +31,21 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/user/signup")
-    public void signUp(@Valid @RequestBody UserRequestDto userRequestDto, BindingResult bindingResult, HttpServletResponse res) throws IOException {
-        // 1. Validation 예외처리
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if (fieldErrors.size() > 0) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()) {
-                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-                throw new IllegalArgumentException(
-                        messageSource.getMessage(
-                                "no.match.structure.signup",
-                                new String[]{fieldError.getField()},
-                                "No Match Structure Signup",
-                                Locale.getDefault() // 국제화하는 것임.
-                        )
-                );
-            }
-        }else {
-            // 2. userService 에서 signup 하고
-            // 3. HttpServletResponse 에 (msg, status set 하기)
-            userService.signUp(userRequestDto,res);
-        }
+    public void signUp(@Valid @RequestBody UserRequestDto userRequestDto, HttpServletResponse res) throws IOException {
+        userService.signUp(userRequestDto,res);
+    }
+
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<RestApiResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        RestApiResponseDto restApiException = new RestApiResponseDto(HttpStatus.BAD_REQUEST.value(),ex.getBindingResult().getAllErrors().get(0).getDefaultMessage(),null );
+        // 위 문장에 check 하고 디버깅 하면 defaultMessage 위치를 알 수 있음.
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
     }
 
 }
--- src/main/java/com/sparta/myblog/dto/UserRequestDto.java
+++ src/main/java/com/sparta/myblog/dto/UserRequestDto.java
@@ -10,12 +10,12 @@ import lombok.Setter;
 public class UserRequestDto {
     // login and sing up Request DTO
 
-    @Pattern(regexp = "^[a-z0-9]{4,10}$" , message = "정규식에 맞지 않습니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$" , message = "username이 정규식에 맞지 않습니다.")
     @NotBlank
     private String username;
 
 
-    @Pattern(regexp = "^[a-zA-Z0-9/[\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]/g]{8,15}$", message = "정규식에 맞지 않습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9/[\\{\\}\\[\\]\\/?.,;:|\\)*~`!^\\-_+<>@\\#$%&\\\\\\=\\(\\'\\\"]/g]{8,15}$", message = "비밀번호가 정규식에 맞지 않습니다.")
     @NotBlank
     private String password;
 
